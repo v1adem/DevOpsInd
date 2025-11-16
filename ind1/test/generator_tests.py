80f887b9553a121429d52e5542a1528af0541205
v1adem <vladem255@gmail.com>
2025-11-16 15:01:25 +0200

    feat: change output to english as in new version of string_operations

--- ind1/test/generator_tests.py
+++ ind1/test/generator_tests.py
@@ -4,7 +4,7 @@ from ..generator import even_odd_generator
 class GeneratorTests(unittest.TestCase):
     def test_even_odd_generator(self):
         generator = even_odd_generator()
-        self.assertEqual(next(generator), "Парне")
-        self.assertEqual(next(generator), "Непарне")
-        self.assertEqual(next(generator), "Парне")
-        self.assertEqual(next(generator), "Непарне")
+        self.assertEqual(next(generator), "Even")
+        self.assertEqual(next(generator), "Odd")
+        self.assertEqual(next(generator), "Even")
+        self.assertEqual(next(generator), "Odd")
